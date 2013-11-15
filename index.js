f8364e96820daa6b8d8209e7e437e24a4625557a
psirenny <djtorres0@gmail.com>
2013-11-15 08:02:41 -0500

    refactor

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ module.exports = function (options) {
 
     model.fetch($private, $public, function (err) {
       if (err) return next(err);
-      var email = $private.get('local.email');
+      var email = $private.get('local.emails.0.value');
       if (!email) return next();
 
       gravitate.profile.data(email, function (err, data) {
-        if (!err) $public.set('local.gravatar', data.entry[0]);
+        if (!err) $public.set('gravatar', data.entry[0]);
         next();
       });
     });
