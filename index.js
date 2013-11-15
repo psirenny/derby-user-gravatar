abd775e42e7d3a6d64ae9b2003d97e56c95d6ab9
psirenny <djtorres0@gmail.com>
2013-11-15 08:56:04 -0500

    add license

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var gravitate = require('gravitate');
 
-module.exports = function (options) {
+module.exports = function (store, options) {
   return function (req, res, next) {
     if (req.get('Content-Type')) return next();
 
     var model = req.getModel()
       , userId = model.get('_session.user.id')
-      , $private = model.at('usersPrivate.' + userId)
-      , $public = model.at('usersPublic.' + userId);
+      , $public = model.at('usersPublic.' + userId)
+      , $private = model.at('usersPrivate.' + userId);
 
-    model.fetch($private, $public, function (err) {
+    model.fetch($public, $private, function (err) {
       if (err) return next(err);
       var email = $private.get('local.emails.0.value');
       if (!email) return next();
